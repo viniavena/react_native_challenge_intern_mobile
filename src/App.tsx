6994c063bd7a835c039afb11d324c58364c5fbdb
Vinicius Avena <vini.avena@live.com>
2022-02-11 14:47:22 -0300

    liking custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -16,54 +16,17 @@ const App = () => {
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <Text style={styles.sectionTitle}>Sanar App</Text>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionDescription}>
-          Acessos a milhares de artigos online.
-        </Text>
-        <View style={styles.image}>
-          <SanarLogo />
-        </View>
-        <View style={styles.button}>
-          <Button title="Começar" />
-        </View>
-      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: '#338BFF',
-    alignSelf: 'center',
-  },
-  highlight: {
-    fontWeight: '900',
-  },
-  image: {
-    paddingTop: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    paddingTop: 320,
-  },
+
 });
 
 export default App;
