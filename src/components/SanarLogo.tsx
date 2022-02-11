2373e1297da681aab84081a8ad5a3e2a7bdd1d65
Vinicius Avena <vini.avena@live.com>
2022-02-11 14:23:59 -0300

    organizing files & folders :truck:

--- src/components/SanarLogo.tsx
+++ src/components/SanarLogo.tsx
@@ -5,7 +5,7 @@ class SanarLogo extends Component {
   render() {
     return (
       <Image
-        source={require('./logo-sanar.png')}
+        source={require('../assets/images/logo-sanar.png')}
         style={{width: 152, height: 45}}
       />
     );
